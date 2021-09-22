2c045024198504c9bcec725bb09c5deee04f03ee
yauheni <Yauheni.conn@gmail.com>
2021-09-22 23:59:08 +0300

    debug - completed price = hull and prefix code

--- src/shops/spiders/21vek.py
+++ src/shops/spiders/21vek.py
@@ -5,24 +5,29 @@ from shops.items import ProductItem
 
 
 class VekSpider(Spider):
-    name = 'vek_spider'
-    allowed_domains = ['www.21vek.by']
-    start_urls = ['https://www.21vek.by/notebooks',
-                  'https://www.21vek.by/tv']
+    name = "vek_spider"
+    allowed_domains = ["www.21vek.by"]
+    start_urls = ["https://www.21vek.by/notebooks", "https://www.21vek.by/tv"]
 
     def start_requests(self):
         for url in self.start_urls:
-            yield XmlRpcRequest(url=url, callback=self.parse_pages, cb_kwargs={"url": url})
+            yield XmlRpcRequest(
+                url=url, callback=self.parse_pages, cb_kwargs={"url": url}
+            )
 
     def parse_pages(self, response, **kwargs):
-        max_page = int(response.xpath("//div[@id='j-paginator']//a[last()-1]/@name").get())
+        max_page = int(
+            response.xpath("//div[@id='j-paginator']//a[last()-1]/@name").get()
+        )
         for page in range(1, max_page):
             yield XmlRpcRequest(url=f"{kwargs['url']}/page:{page}", callback=self.parse)
 
     def parse(self, response, **kwargs):
         for product in response.xpath("//ul[@class='b-result']/li"):
-            yield ProductItem(
-                name=product.css("span.result__name::text").get(),
-                code=product.css("span.g-code::text").get(),
-                price=product.css('span.g-item-data::attr(content)').get()
-            )
+            price = product.css("span.g-item-data::attr(content)").get()
+            if price:
+                yield ProductItem(
+                    name=product.css("span.result__name::text").get(),
+                    code=product.css("span.g-code::text").get().lstrip("код "),
+                    price=price,
+                )
